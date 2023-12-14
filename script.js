cdd01af38efcb266e0caff21a03eccc34f81684d
shakilahfsa <153863016+shakilahfsa@users.noreply.github.com>
2023-12-15 02:24:37 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+// If you have JavaScript code, you can place it here
+// Example: Implementing the glow-on-hover effect
+document.addEventListener('DOMContentLoaded', function() {
+    // JavaScript code goes here
+});
