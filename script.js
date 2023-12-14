25ca5228a492dca859c2a1249b1f0a09c031523d
shakilahfsa <153863016+shakilahfsa@users.noreply.github.com>
2023-12-15 02:34:10 +0600

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// script.js
-// If you have JavaScript code, you can place it here
-// Example: Implementing the glow-on-hover effect
-document.addEventListener('DOMContentLoaded', function() {
-    // JavaScript code goes here
-});
